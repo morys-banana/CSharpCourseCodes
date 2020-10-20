5fcb771d0ca14f5019d239c65ce16ece677a1f07
morys-banana <64895695+morys-banana@users.noreply.github.com>
2020-10-21 01:19:11 +0300

    First CSharp Program
    This is My First Csharp Progarm

---
+++ code1/code1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is first program in CSharp
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+
+        }
+    }
+}
